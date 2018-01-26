59f7f2ce124ac58f162457a63cfd92689612f942
Shang Huang <2260181h@student.gla.ac.uk>
2018-01-26 11:36:12 +0000

    about.html updated

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,5 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
-
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return render(request, 'rango/about.html')
