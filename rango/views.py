2c1aa20dba6269bccc4b395a4786b0856247c594
Shang Huang <2260181h@student.gla.ac.uk>
2018-02-09 17:07:03 +0000

    Chapter 9

--- rango/views.py
+++ rango/views.py
@@ -12,12 +12,16 @@ from django.contrib.auth import logout
 from django.http import HttpResponse
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
